8b8c52398af82e070da177490f083446ae8db945
Michael Solomon <se.michael.solomon@gmail.com>
2021-08-23 21:11:56 +0300

    folder structure

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,18 +1,3 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(App());
-}
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Eshi Blood"),
-        ),
-      ),
-    );
-  }
-}
+// void main() => runApp(App());
